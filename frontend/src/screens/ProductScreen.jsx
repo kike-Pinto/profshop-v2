c71b3f486bb2fa2ec1295067d1494426fccde4c4
kike-pinto <cripintort@gmail.com>
2023-08-17 23:07:00 -0400

    Fetch Products

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+// import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState([])
   const { id: productId } = useParams()
-  const product = products.find((p) => p._id === productId)
+  // const product = products.find((p) => p._id === productId)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`)
+      setProduct(data)
+    }
+    fetchProducts()
+  }, [productId])
+
   console.log(product)
 
   return (
