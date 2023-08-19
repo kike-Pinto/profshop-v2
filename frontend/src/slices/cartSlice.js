5747b4fa29fd613f500b6076e35012c0e1115ee8
kike-pinto <cripintort@gmail.com>
2023-08-19 17:35:21 -0400

    Qty & addToCart & Utils file

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { updateCart } from '../utils/cartUtils'
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
@@ -7,7 +8,28 @@ const initialState = localStorage.getItem('cart')
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    // addToCart is our Redux Action Creator Function
+    // A function that returns an action, i.e an object with a type & payload
+    addToCart: (state, action) => {
+      const item = action.payload
+      // Check if the item is already in the cart
+      const existItem = state.cartItems.find((x) => x._id === item._id)
+
+      // Update the quantity
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        )
+      } else {
+        state.cartItems = [...state.cartItems, item]
+      }
+
+      return updateCart(state)
+    },
+  },
 })
+
+export const { addToCart } = cartSlice.actions
 
 export default cartSlice.reducer
