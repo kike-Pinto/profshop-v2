2703f7559b97f8eeb7d2bd2e44c077694192ab49
kike-pinto <cripintort@gmail.com>
2023-08-19 23:45:02 -0400

    User Email & Password Auth

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ connectDB() // connect to MondoDB
 
 const app = express()
 
+// Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
