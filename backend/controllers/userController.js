2b84271eaf1b32b95bcbdd99f4f491040ef78c43
kike-pinto <cripintort@gmail.com>
2023-08-20 13:12:23 -0400

    User Profile Endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,7 @@ const authUser = asyncHandler(async (req, res) => {
     //   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     // })
 
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -85,14 +85,47 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route  GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('get user profile')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      idAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 })
 
 // @desc   Update user profile
 // @route  PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('update user profile')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    ;(user.name = req.body.name || user.name),
+      (user.email = req.body.email || user.email)
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updateUser = await user.save()
+
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 })
 
 // @desc   Get User
