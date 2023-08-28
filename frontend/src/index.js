3c9e4a057b67db255782cd5ac7fda6325bbb4c65
kike-pinto <cripintort@gmail.com>
2023-08-28 14:23:30 -0400

    List Products For Admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,6 +27,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScrenn'
 import OrderScreen from './screens/OrderScreen'
 import ProfileScrenn from './screens/ProfileScrenn'
 import OrderListScreen from './screens/admin/OrderListScreen'
+import ProductListScreen from './screens/admin/ProductListScreen'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -47,6 +48,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<AdminRoute />}>
         <Route path='/admin/orderlist' element={<OrderListScreen />} />
+        <Route path='/admin/productlist' element={<ProductListScreen />} />
       </Route>
     </Route>
   )
